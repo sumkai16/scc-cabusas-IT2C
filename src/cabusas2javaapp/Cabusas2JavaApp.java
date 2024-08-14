892a39db1f79abf2455b853b386014173b60b2f7
axceelfelis03@gmail.com <kylepardillo55@gmail.com>
2024-08-14 15:28:12 +0800

    first repo ni axci ohaha

---
+++ src/cabusas2javaapp/Cabusas2JavaApp.java
@@ -0,0 +1,14 @@
+
+package cabusas2javaapp;
+
+
+public class Cabusas2JavaApp {
+
+    
+    public static void main(String[] args) {
+        
+        netPay npg = new netPay();
+        npg.getPay();
+    }
+    
+}
